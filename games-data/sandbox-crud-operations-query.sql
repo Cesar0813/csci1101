31cc39a5baecaaa6a6e3a5aff883f45f25891d6b
Cesar Rodriguez <cesar.rodriguez06@utrgv.edu>
2022-12-07 00:56:32 -0600

    Add games data project

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int64_value, bool_value, string_value) VALUES (420420420, 0, 'Watching the recording:) ');
+
+--UPDATE sandbox SET double_value = -2.1543054 WHERE id = 120;
+
+--DELETE FROM sandbox WHERE id = 123;
+
+SELECT * FROM sandbox;
